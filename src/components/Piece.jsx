3fee54d42391b5b446d820f1a5a72bfcd3181223
jsmithb117 <jsmithb117@gmail.com>
2021-09-17 09:18:40 -0700

    Added color to pieces and cover/uncover classes

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Piece = (props) => (
-  <button>
+const Piece = (props) => {
+  const buttonColor = props.piece.val === 0 ? null :
+    props.piece.val === 1 ? 'blue' :
+    props.piece.val === 2 ? 'green' :
+    props.piece.val === 3 ? 'red' :
+    props.piece.val === 4 ? 'purple' :
+    props.piece.val === 5 ? 'maroon' :
+    props.piece.val === 6 ? 'turquoise' :
+    props.piece.val === 7 ? 'black' :
+    props.piece.val === 8 ? 'gray' :
+    undefined;
+
+  const uncoveredClassName = `uncovered piece row${props.piece.row} col${props.piece.col} ${buttonColor}`;
+  const coveredClassName = `covered piece row${props.piece.row} col${props.piece.col}`;
+  return (
+  <button className={uncoveredClassName}>
     {props.piece.val}
   </button>
-);
+  )
+};
 
 export default Piece;
