9ca7416b1a95605df132a1c5c6de29e2b2e35bbc
jsmithb117 <jsmithb117@gmail.com>
2021-09-17 09:59:53 -0700

    Installed redux, react-redux, created store

---
+++ src/features/store.js
@@ -0,0 +1,13 @@
+import reducer from './reducer';
+import { createStore } from 'redux';
+import boardCreator from '../components/boardCreator';
+
+const initialState = {
+  board: boardCreator(),
+  win: false,
+  loss: false,
+};
+
+const store = createStore(reducer, initialState);
+
+export default store;
