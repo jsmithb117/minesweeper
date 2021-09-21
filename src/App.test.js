36eb45c10df6704fc8b5dddc8fda5ab51afae2c8
jsmithb117 <jsmithb117@gmail.com>
2021-09-21 16:29:30 -0700

    Clarified what was being tested

--- src/App.test.js
+++ src/App.test.js
@@ -16,19 +16,21 @@ describe('App', () => {
   const store = mockStore(initialTestState);
   const wrapper = mount(
     <Provider store={store}>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </Provider>
   );
   it('should render 100 Piece components on testBoard', () => {
     expect(wrapper.find(Piece).length).toEqual(100)
   });
-  it('should render 10 Piece components whose val is set as \'X\'', () => {
+  it('should render 10 Piece components whose val is set as \'X\' on testBoard', () => {
     expect(wrapper.findWhere(node => node?.props()?.piece?.val === 'X').length).toEqual(10);
   });
-  it('should render 10 Piece components whose \'markedAsMine\' property is set to true', () => {
+  it('should render 10 Piece components whose \'markedAsMine\' property is set to true on testBoard', () => {
     expect(wrapper.findWhere(node => node?.props()?.piece?.markedAsMine === true).length).toEqual(10);
   });
-  it('should render 20 Piece components with the \'uncovered\' property set', () => {
+  it('should render 20 Piece components with the \'uncovered\' property set on testBoard', () => {
     expect(wrapper.findWhere(node => node?.props()?.piece?.uncovered).length).toEqual(20);
   });
 });
