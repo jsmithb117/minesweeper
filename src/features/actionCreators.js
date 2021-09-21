3e8a3b89dea071ae68aa3e3eae2d04ef4ffad93c
jsmithb117 <jsmithb117@gmail.com>
2021-09-21 16:31:27 -0700

    Added prop validation and error handling to action creators

--- src/features/actionCreators.js
+++ src/features/actionCreators.js
@@ -2,6 +2,17 @@ export const LEFTCLICK = 'LEFTCLICK';
 export const RIGHTCLICK = 'RIGHTCLICK';
 
 export const leftClick = (piece) => {
+  const val = piece.hasOwnProperty('val');
+  const uncovered = piece.hasOwnProperty('uncovered');
+  const markedAsMine = piece.hasOwnProperty('markedAsMine');
+  const row = piece.hasOwnProperty('row');
+  const col = piece.hasOwnProperty('col');
+
+  if (!val || !uncovered || !markedAsMine || !row || !col) {
+    const error = new Error('leftClick action creator called with improper props')
+    console.error(error);
+    return error;
+  }
   return {
     type: LEFTCLICK,
     payload: { piece },
@@ -9,6 +20,17 @@ export const leftClick = (piece) => {
 };
 
 export const rightClick = (piece) => {
+  const val = piece.hasOwnProperty('val');
+  const uncovered = piece.hasOwnProperty('uncovered');
+  const markedAsMine = piece.hasOwnProperty('markedAsMine');
+  const row = piece.hasOwnProperty('row');
+  const col = piece.hasOwnProperty('col');
+
+  if (!val || !uncovered || !markedAsMine || !row || !col) {
+    const error = new Error('rightClick action creator called with improper props')
+    console.error(error);
+    return error;
+  }
   return {
     type: RIGHTCLICK,
     payload: { piece },
