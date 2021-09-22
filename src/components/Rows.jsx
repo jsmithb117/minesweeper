f80a3c7ceef5d6ac19a46b77acf0c18a0c726eff
jsmithb117 <jsmithb117@gmail.com>
2021-09-22 11:07:13 -0700

    Adds win and loss props to each Piece for testing purposes

--- src/components/Rows.jsx
+++ src/components/Rows.jsx
@@ -4,9 +4,11 @@ import { useSelector } from 'react-redux';
 
 const Rows = (props) => {
   const board = useSelector((state) => state.board);
+  const win = useSelector(state => state.win);
+  const loss = useSelector(state => state.loss);
   const rowsMap = board.map((row, index) => {
     return (
-      <Row row={row} key={`row${index}`} />
+      <Row row={row} key={`row${index}`} win={win} loss={loss} />
     )
   });
 
