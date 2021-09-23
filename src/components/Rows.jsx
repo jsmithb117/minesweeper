f6a383c3fbefa1174ee1bb7f5badc8647ca42633
jsmithb117 <jsmithb117@gmail.com>
2021-09-23 15:05:58 -0700

    Refactored Rows component to TypeScript

--- src/components/Rows.jsx
+++ src/components/Rows.jsx
@@ -3,10 +3,17 @@ import Row from './Row';
 import { useSelector } from 'react-redux';
 
 const Rows = () => {
-  const board = useSelector((state) => state.board);
-  const win = useSelector(state => state.win);
-  const loss = useSelector(state => state.loss);
-  const rowsMap = board.map((row, index) => {
+  const board = useSelector((state: IRootState) => state.board);
+  const win = useSelector((state: IRootState) => state.win);
+  const loss = useSelector((state: IRootState) => state.loss);
+  const rowsMap = board.map((row: Array<{
+    val: number,
+    isMine: boolean,
+    uncovered: boolean,
+    markedAsMine: boolean,
+    row: number,
+    col: number,
+  }>, index: number) => {
     return (
       <Row row={row} key={`row${index}`} win={win} loss={loss} />
     );
