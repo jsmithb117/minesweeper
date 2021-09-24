fbfb9898c4d2bc61b7db30d7237f23d58ff3e9b3
jsmithb117 <jsmithb117@gmail.com>
2021-09-24 11:26:13 -0700

    Refactors initialState to Typescript

---
+++ src/features/initialState.ts
@@ -0,0 +1,22 @@
+import boardCreator, { Board } from './boardCreator';
+
+export type State = {
+  board: Board,
+  win: boolean,
+  loss: boolean,
+};
+
+const initialState = (test: boolean = false) => {
+  const state: { win: boolean, loss: boolean } = {
+    win: false,
+    loss: false,
+  };
+  if (test) {
+    return { ...state, board: boardCreator(10, 10, 10, true) }
+  }
+  const newState: State = { ...state, board: boardCreator() }
+  return newState;
+
+};
+
+export default initialState;
