3afba0587e5ce99ee840117885c427ca4e3c4733
jsmithb117 <jsmithb117@gmail.com>
2021-09-24 11:18:57 -0700

    Refactored checkWin to TypeScript

--- src/features/checkWin.ts
+++ src/features/checkWin.ts
@@ -1,7 +1,8 @@
-const checkWin = (board) => {
+import { Board } from './boardCreator';
+
+const checkWin = (board: Board) => {
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
-      //if piece is not uncovered AND piece is not a mine
       if (!board[i][j].uncovered && !board[i][j].isMine) {
         return false;
       }
