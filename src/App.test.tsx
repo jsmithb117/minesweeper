77c0cc2be4dc49267208c06c5b4cd824271b68c7
jsmithb117 <jsmithb117@gmail.com>
2021-09-27 16:34:36 -0700

    Refactors App test to TypeScript

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { mount, ReactWrapper } from 'enzyme';
 import { createStore } from 'redux';
 
 import reducer from './features/reducer';
@@ -14,7 +14,7 @@ const initialTestState = initialState(testBoard);
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('App', () => {
-  let store, wrapper;
+  let store, wrapper: ReactWrapper;
   beforeEach(() => {
     store = createStore(reducer, initialTestState);
     wrapper = mount(
@@ -26,70 +26,76 @@ describe('App', () => {
     );
   })
 
-  const findPiece = (node, row, col) => node?.props()?.piece?.row === row && node?.props()?.piece?.col === col;
+  const findPieceAtRowCol = (row: number, col: number) => {
+    return wrapper.find(Piece).filterWhere((n) => {
+      return n.props().piece.row === row && n.props().piece.col === col;
+    });
+  };
 
   it('should render 100 Piece components on testBoard', () => {
     expect(wrapper.find(Piece).length).toEqual(100)
   });
   it('should render 10 Piece components whose isMine property is set to true on testBoard', () => {
-    expect(wrapper.findWhere(node => node?.props()?.piece?.isMine).length).toEqual(10);
+    expect(wrapper.find(Piece).filterWhere((n) => n.props().piece.isMine).length).toBe(10);
   });
   it('should render 10 Piece components whose \'markedAsMine\' property is set to true on testBoard', () => {
-    expect(wrapper.findWhere(node => node?.props()?.piece?.markedAsMine === true).length).toEqual(10);
+    expect(wrapper.find(Piece).filterWhere((n) => n.props().piece.markedAsMine).length).toBe(10);
   });
   it('should render 10 Piece components whose \'uncovered\' property is set to true on testBoard', () => {
-    expect(wrapper.findWhere(node => node?.props()?.piece?.uncovered).length).toEqual(10);
+    expect(wrapper.find(Piece).filterWhere((n) => n.props().piece.uncovered).length).toBe(10);
   });
   it('should not change \'uncovered\' property when markedAsMine is true', () => {
-    const button = wrapper.findWhere(node => findPiece(node, 1, 1));
+    const button = findPieceAtRowCol(1, 1);
     expect(button.props().piece.uncovered).toBe(false);
     expect(button.props().piece.markedAsMine).toBe(false);
     button.simulate('contextmenu', { ...button.props() });
-    const button2 = wrapper.findWhere(node => findPiece(node, 1, 1));
+    const button2 = findPieceAtRowCol(1,1);
     expect(button2.props().piece.markedAsMine).toBe(true);
     button2.simulate('click', { ...button2.props() });
-    const button3 = wrapper.findWhere(node => findPiece(node, 1, 1));
+    const button3 = findPieceAtRowCol(1,1);
     expect(button3.props().piece.uncovered).toBe(false);
   });
   it('should toggle \'markedAsMine\' property when piece is right clicked', () => {
-    const button = wrapper.findWhere(node => findPiece(node, 9, 9));
+    const button = findPieceAtRowCol(9, 9);
     expect(button.props().piece.markedAsMine).toBe(false);
+    button.simulate('contextmenu', { ...button.props().piece });
+    const button2 = findPieceAtRowCol(9, 9);
+    expect(button2.props().piece.markedAsMine).toBe(true);
     button.simulate('contextmenu', { ...button.props() });
-    expect(wrapper.findWhere(node => findPiece(node, 9, 9)).props().piece.markedAsMine).toBe(true);
-    button.simulate('contextmenu', { ...button.props() });
-    expect(wrapper.findWhere(node => findPiece(node, 9, 9)).props().piece.markedAsMine).toBe(false);
+    expect(findPieceAtRowCol(9, 9).props().piece.markedAsMine).toBe(false);
   });
   it('should set \'uncovered\' property to true when piece is left clicked', () => {
-    const button = wrapper.findWhere(node => findPiece(node, 9, 9));
+    const button = findPieceAtRowCol(9, 9);
     expect(button.props().piece.uncovered).toBe(false);
     button.simulate('click', { ...button.props() });
-    expect(wrapper.findWhere(node => findPiece(node, 9, 9)).props().piece.uncovered).toBe(true);
+    expect(findPieceAtRowCol(9, 9).props().piece.uncovered).toBe(true);
   });
   it('should set \'loss\' property to true when an unset mine is clicked', () => {
-    const button = wrapper.findWhere(node => findPiece(node, 0, 0));
+    const button = findPieceAtRowCol(0, 0);
     expect(button.props().piece.uncovered).toBe(false);
     expect(button.props().piece.markedAsMine).toBe(true);
     button.simulate('contextmenu', { ...button.props().piece });
-    expect(wrapper.findWhere(node => findPiece(node, 0, 0)).props().piece.markedAsMine).toBe(false);
+    const button2 = findPieceAtRowCol(0, 0);
+    expect(button2.props().piece.markedAsMine).toBe(false);
     button.simulate('click', { ...button.props().piece });
-    expect(wrapper.findWhere(node => findPiece(node, 0, 0)).props().loss).toBe(true);
+    expect(findPieceAtRowCol(0, 0).props().loss).toBe(true);
   });
   it('should set \'win\' property to true when all pieces whose value is not \'X\' are uncovered', () => {
-    const buttons = wrapper.findWhere(node => node?.props()?.piece?.markedAsMine === false);
+    const buttons = wrapper.find(Piece).filterWhere((n) => n.props().piece.markedAsMine === false);
     buttons.forEach((node) => {
       node.simulate('click', { ...node?.props()?.piece });
     });
     expect(wrapper.find(Piece).at(0).props().win).toBe(true);
   });
   it('should set \'loss\' property to true when an uncovered mine is clicked', () => {
-    const button = wrapper.findWhere(node => findPiece(node, 0, 0));
+    const button = findPieceAtRowCol(0, 0);
     expect(button.props().piece.markedAsMine).toBe(true);
     button.simulate('contextmenu', { ...button.props().piece });
-    const button2 = wrapper.findWhere(node => findPiece(node, 0, 0));
+    const button2 = findPieceAtRowCol(0, 0);
     expect(button2.props().piece.markedAsMine).toBe(false);
     expect(button2.props().loss).toBe(false);
     button2.simulate('click', { ...button2.props().piece });
-    expect(wrapper.findWhere(node => findPiece(node, 0, 0)).props().loss).toBe(true);
+    expect(findPieceAtRowCol(0, 0).props().loss).toBe(true);
   });
 });
 
