ef1cc45bd1dbf0b6b8e88c16a7e26ee6c2f06073
jsmithb117 <jsmithb117@gmail.com>
2021-09-29 15:38:03 -0700

    Handles new action types

--- src/features/reducer.ts
+++ src/features/reducer.ts
@@ -2,53 +2,94 @@ import produce, { Draft } from 'immer';
 import {
   LEFTCLICK,
   RIGHTCLICK,
-  IAction,
+  RESETTIME,
+  INCREMENTTIME,
+  SETLENGTH,
+  SETWIDTH,
+  SETMINES,
+  NEWBOARD,
+  IActions,
 } from './actionCreators';
 import zeroFinder from './zeroFinder';
 import checkWin from './checkWin';
 import { IState } from './initialState';
-import { IPiece } from './boardCreator';
+import boardCreator, { IPiece, backupPiece } from './boardCreator';
 
-const reducer = (state: any, action: IAction) => {
+const reducer = (state: any, action: IActions) => {
   if (state.win || state.loss) {
     return state;
   }
-  const piece: IPiece = action?.payload?.piece;
-  const row: number = piece?.row;
-  const col: number = piece?.col;
+  const piece: IPiece = action?.payload?.piece || backupPiece;
+  const row: number = piece.row;
+  const col: number = piece.col;
+
 
   if (action.type === LEFTCLICK) {
-    const nextState = produce(state, (draftState: Draft<IState>) => {
+    return produce(state, (draftState: Draft<IState>) => {
       if (!piece.uncovered && !piece.markedAsMine) {
         draftState.board = zeroFinder(row, col, draftState.board);
       }
       if (piece.isMine && !piece.markedAsMine) {
         draftState.loss = true;
         return draftState;
-      } else if (checkWin(draftState.board)){
+      } else if (checkWin(draftState.board)) {
         draftState.win = true;
         return draftState;
       }
     });
-
-    return nextState;
   }
-
   if (action.type === RIGHTCLICK) {
-    const nextState = produce(state, (draftState: Draft<IState>) => {
-      if (!action.payload.piece.uncovered) {
-        draftState.board[row][col].markedAsMine = !action.payload.piece.markedAsMine;
+    return produce(state, (draftState: Draft<IState>) => {
+      if (!piece.uncovered) {
+        draftState.board[row][col].markedAsMine = !piece.markedAsMine;
       }
     });
-
-    return nextState;
+  }
+  if (action.type === RESETTIME) {
+    return produce((draft) => {
+      draft.time = 0;
+    });
+  }
+  if (action.type === INCREMENTTIME) {
+    return produce((draft) => {
+      draft.time += 1;
+    });
+  }
+  if (action.type === SETLENGTH) {
+    return produce((draft) => {
+      draft.length = action.payload.length;
+      return draft;
+    });
+  }
+  if (action.type === SETWIDTH) {
+    return produce((draft) => {
+      draft.width = action.payload.length;
+      return draft;
+    });
+  }
+  if (action.type === SETMINES) {
+    return produce((draft) => {
+      draft.mines = action.payload.mines;
+      return draft;
+    });
+  }
+  if (action.type === NEWBOARD) {
+    return produce((draft) => {
+      draft.board = boardCreator(draft.length, draft.width, draft.mines, false);
+      return draft;
+    })
   }
 
-  if (action.type.slice(0,7) !== '@@redux') {
+  if (action.type.slice(0, 7) !== '@@redux') {
     console.error('Action not found: ', action);
   }
 
   return state;
 };
 
+// const formReducer = (state: IState, action: IActions) => {
+
+// };
+
+// export default clickReducer;
 export default reducer;
