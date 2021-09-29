d686ffa9a16fb3b148511213157ae2dfab4304bc
jsmithb117 <jsmithb117@gmail.com>
2021-09-29 15:40:11 -0700

    Modifies initialState for new action types

--- src/features/initialState.ts
+++ src/features/initialState.ts
@@ -10,28 +10,30 @@ interface IInitialState {
   time: number,
   length: number,
   width: number,
-  totalMines: number,
+  mines: number,
   win: boolean,
   loss: boolean,
 };
 
-const initialState = (test: boolean = false) => {
-  const state: IInitialState = {
-    piecesMarkedAsMine: 0,
-    minesDisplay: 0,
-    time: 0,
-    length: 10,
-    width: 10,
-    totalMines: 10,
-    win: false,
-    loss: false,
-  };
+const defaultState: IInitialState = {
+  piecesMarkedAsMine: 0,
+  minesDisplay: 0,
+  time: 0,
+  length: 10,
+  width: 10,
+  mines: 10,
+  win: false,
+  loss: false,
+};
+
+const initialStateCreator = (test: boolean = false, state: IInitialState = defaultState) => {
+
   if (test) {
-    const newState: IState = { ...state, board: boardCreator(state.length, state.width, state.totalMines, true) }
+    const newState: IState = { ...state, board: boardCreator(state.length, state.width, state.mines, true) }
     return newState;
   }
   const newState: IState = { ...state, board: boardCreator() }
   return newState;
 };
 
-export default initialState;
+export default initialStateCreator;
