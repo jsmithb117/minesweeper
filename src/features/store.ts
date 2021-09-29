2359a61503b01d1e1221d9f08338a94ff0dc4554
jsmithb117 <jsmithb117@gmail.com>
2021-09-29 15:39:12 -0700

    Eliminates duplicate interface and state creation logic

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,19 +1,8 @@
-import reducer from './reducer';
+import reducer  from './reducer';
 import { createStore } from 'redux';
-import boardCreator, { Board } from './boardCreator';
+import initialStateCreator from './initialState';
 
-interface IInitialState {
-  board: Board,
-  win: boolean,
-  loss: boolean
-};
-
-const initialState: IInitialState = {
-  board: boardCreator(10, 10, 10, false),
-  win: false,
-  loss: false,
-};
-
+const initialState = initialStateCreator(false, );
 const store = createStore(reducer, initialState);
 
 export default store;
