28b33888a4c5e455c49f2fe6ca01660bce23bfe8
jsmithb117 <jsmithb117@gmail.com>
2021-09-29 11:30:16 -0700

    Adds additional state for Forms components

--- src/features/initialState.ts
+++ src/features/initialState.ts
@@ -1,18 +1,34 @@
 import boardCreator, { Board } from './boardCreator';
 
-export interface IState {
+export interface IState extends IInitialState {
   board: Board,
+};
+
+interface IInitialState {
+  piecesMarkedAsMine: number,
+  minesDisplay: number,
+  time: number,
+  length: number,
+  width: number,
+  totalMines: number,
   win: boolean,
   loss: boolean,
 };
 
 const initialState = (test: boolean = false) => {
-  const state: { win: boolean, loss: boolean } = {
+  const state: IInitialState = {
+    piecesMarkedAsMine: 0,
+    minesDisplay: 0,
+    time: 0,
+    length: 10,
+    width: 10,
+    totalMines: 10,
     win: false,
     loss: false,
   };
   if (test) {
-    return { ...state, board: boardCreator(10, 10, 10, true) }
+    const newState: IState = { ...state, board: boardCreator(state.length, state.width, state.totalMines, true) }
+    return newState;
   }
   const newState: IState = { ...state, board: boardCreator() }
   return newState;
