6c295cd789d9721dd380fbcba7f098981f670326
jsmithb117 <jsmithb117@gmail.com>
2021-10-01 12:21:42 -0700

    Handles SETMINESDISPLAY action

--- src/features/reducer.ts
+++ src/features/reducer.ts
@@ -7,17 +7,17 @@ import {
   SETLENGTH,
   SETWIDTH,
   SETMINES,
-  NEWBOARD,
   IFormPayload,
   IClickAction,
   REVERTBOARD,
   INCREMENTMINESDISPLAY,
-  DECREMENTMINESDISPLAY
+  DECREMENTMINESDISPLAY,
+  SETMINESDISPLAY
 } from './actionCreators';
 import zeroFinder from './zeroFinder';
 import checkWin from './checkWin';
 import { IInitialState, IClickState, IFormState, initialState } from './initialState';
-import boardCreator, { IPiece, backupPiece } from './boardCreator';
+import { IPiece, backupPiece } from './boardCreator';
 
 export const clickReducer = (state: IInitialState = initialState, action: IClickAction) => {
   if (state?.click?.win || state?.click?.loss) {
@@ -54,14 +54,14 @@ export const clickReducer = (state: IInitialState = initialState, action: IClick
       }
     });
   }
-    if (action.type === NEWBOARD) {
-    return produce((draft) => {
-      draft.board = boardCreator(draft.length, draft.width, draft.mines, false);
-      return draft;
-    });
-  }
+  //   if (action.type === NEWBOARD) {
+  //   return produce(state, (draft: Draft<IClickState>) => {
+  //     draft.board = boardCreator(draft.length, draft.width, draft.mines, false);
+  //     return draft;
+  //   });
+  // }
   if (action.type === REVERTBOARD) {
-    return produce((draft) => {
+    return produce(state, (draft: Draft<IClickState>) => {
       draft.board = draft.originalBoard;
       return draft;
     });
@@ -71,46 +71,51 @@ export const clickReducer = (state: IInitialState = initialState, action: IClick
 
 export const formReducer = (state: IInitialState = initialState, action: IFormPayload) => {
   if (action.type === RESETTIME) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.time = 0;
       return draft;
     });
   }
   if (action.type === INCREMENTTIME) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.time += 1;
     });
   }
   if (action.type === SETLENGTH) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.length = action.payload;
       return draft;
     });
   }
   if (action.type === SETWIDTH) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.width = action.payload;
       return draft;
     });
   }
   if (action.type === SETMINES) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.mines = action.payload
       return draft;
     });
   }
   if (action.type === INCREMENTMINESDISPLAY) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.minesDisplay += 1;
       return draft;
     });
   }
   if (action.type === DECREMENTMINESDISPLAY) {
-    return produce((draft: Draft<IFormState>) => {
+    return produce(state, (draft: Draft<IFormState>) => {
       draft.minesDisplay -= 1;
       return draft;
     }
     )};
-
+  if (action.type === SETMINESDISPLAY) {
+    return produce(state, (draft: Draft<IFormState>) => {
+      draft.minesDisplay = action.payload;
+      return draft;
+    });
+  }
   return state;
 };
