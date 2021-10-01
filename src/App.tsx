0c0a36b1589cbf8754c527a98b3e6d5de386e8d8
jsmithb117 <jsmithb117@gmail.com>
2021-10-01 12:22:41 -0700

    Sets minesDisplay on initial render and when state.form.mines changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css';
 import Rows from './components/Rows';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-import { useAppSelector } from './features/hooks';
+import { useAppSelector, useAppDispatch } from './features/hooks';
 import { IInitialState } from './features/initialState';
+import { useEffect } from 'react';
+import { setMinesDisplay } from './features/actionCreators';
 
 function App() {
   document.addEventListener('contextmenu', event => event.preventDefault());
   const win = useAppSelector((state: IInitialState) => state.click.win);
   const loss = useAppSelector((state: IInitialState) => state.click.loss);
+  const mines = useAppSelector((state: IInitialState) => state.form.mines);
+  const dispatch = useAppDispatch();
 
   const boardColor = win ? 'green'
     : loss ? 'red'
@@ -15,6 +19,9 @@ function App() {
 
   const className = 'minesweeper'.concat(boardColor);
 
+  useEffect(() => {
+    dispatch(setMinesDisplay(mines));
+  }, [dispatch, mines])
   return (
     <div className={className}>
       <HelmetProvider>
