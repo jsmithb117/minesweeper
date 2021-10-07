fc56a846944befdc0443a4455e2534b682aa3783
jsmithb117 <jsmithb117@gmail.com>
2021-10-07 11:45:58 -0700

    Removed console.logs

--- src/features/pieceClass.ts
+++ src/features/pieceClass.ts
@@ -2,7 +2,6 @@ import buttonColor from './buttonColor';
 import { IPiece } from './boardCreator';
 
 const pieceClass = (piece: IPiece, width: number, length: number) => {
-  console.log('pieceClass: width: length: ', width, ' ', length);
   const className: string = `piece width${width} length${length}`;
 
   if (!piece.uncovered && !piece.markedAsMine) {
