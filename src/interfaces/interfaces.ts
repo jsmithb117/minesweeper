eba1ca0890caee8c8ebbbfe13c45b00f0a25516a
jsmithb117 <jsmithb117@gmail.com>
2021-10-19 16:18:48 -0700

    Creates new type for actions

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,3 +1,5 @@
+import { Board } from '../features/boardCreator';
+
 export interface IActionNoPayload {
   type: string,
 };
@@ -53,4 +55,97 @@ export interface DisplayState {
     mines: number,
     paused: boolean,
   },
+};
+
+export interface IClickState {
+  piecesMarkedAsMine: number,
+  board: Board,
+  originalBoard: Board,
+  win: boolean,
+  loss: boolean,
+};
+
+export interface IFormState {
+  minesDisplay: number,
+  time: number,
+  length: number,
+  width: number,
+  mines: number,
+  paused: boolean,
+};
+
+export interface IScore {
+  username: string,
+  seconds: number,
+  date: Date,
+};
+
+export interface IInitialState {
+  click: {
+    piecesMarkedAsMine: number,
+    board: Board,
+    originalBoard: Board,
+    win: boolean,
+    loss: boolean,
+  },
+  form: {
+    minesDisplay: number,
+    time: number,
+    length: number,
+    width: number,
+    mines: number,
+    paused: boolean,
+  },
+  stats: {
+    bestBeginnerScore: IScore,
+    bestIntermediateScore: IScore,
+    bestExpertScore: IScore,
+    beginnerScores: IScore[],
+    intermediateScores: IScore[],
+    expertScores: IScore[],
+    totalGamesPlayed: number,
+  }
+};
+
+export interface IAction {
+  type: String | null,
+  payload: {
+    val: Number | null,
+    isMine: Boolean | null,
+    uncovered: Boolean | null,
+    markedAsMine: Boolean | null,
+    loser: Boolean | null,
+    row: Number | null,
+    col: Number | null,
+    piece: {
+      val: Number | null,
+      isMine: Boolean | null,
+      uncovered: Boolean | null,
+      markedAsMine: Boolean | null,
+      loser: Boolean | null,
+      row: Number | null,
+      col: Number | null,
+    },
+    length: Number | null,
+    width: Number | null,
+    mines: Number | null,
+    minesDisplay: Number | null,
+    bestBeginnerScore: IScore | null,
+    bestIntermediateScore: IScore | null,
+    bestExpertScore: IScore | null,
+    beginnerScores: IScore[] | null,
+    intermediateScores: IScore[] | null,
+    expertScores: IScore[] | null,
+    totalGamesPlayed: number | null,
+  }
+};
+
+export interface IStatsUpdate {
+  bestBeginnerScore: IScore,
+  bestIntermediateScore: IScore,
+  bestExpertScore: IScore,
+  beginnerScores: IScore[],
+  intermediateScores: IScore[],
+  expertScores: IScore[],
+  totalGamesPlayed: number,
 };
