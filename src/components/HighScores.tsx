8247571a53c7d71b761bdcf23ab10b880f63c2f4
jsmithb117 <jsmithb117@gmail.com>
2021-11-08 15:57:36 -0800

    Refactors client from RESTful fetch to GraphQL fetch for userData

--- src/components/HighScores.tsx
+++ src/components/HighScores.tsx
@@ -23,12 +23,56 @@ const HighScores = () => {
   });
 
   const userData = useQuery(['user', { username }], async () => {
-    const userOpts = {
+    const graphOpts: any = {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
-    };
-    const response = await fetch(URI.concat('/user'), userOpts);
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query: '\n' +
+          '  mutation {\n' +
+          '    completed(username:"user1", seconds: 14, date: "1636142219373", difficulty: "Expert") {\n' +
+          '      best_beginner_score{\n' +
+          '        username\n' +
+          '        seconds\n' +
+          '        date\n' +
+          '      }\n' +
+          '      best_intermediate_score{\n' +
+          '        username\n' +
+          '        seconds\n' +
+          '        date\n' +
+          '      }\n' +
+          '      best_expert_score{\n' +
+          '        username\n' +
+          '        seconds\n' +
+          '        date\n' +
+          '      }\n' +
+          '      beginner_scores{\n' +
+          '        username\n' +
+          '        seconds\n' +
+          '        date\n' +
+          '      }\n' +
+          '      intermediate_scores{\n' +
+          '        username\n' +
+          '        seconds\n' +
+          '        date\n' +
+          '      }\n' +
+          '      expert_scores{\n' +
+          '        username\n' +
+          '        seconds\n' +
+          '        date\n' +
+          '      }\n' +
+          '      total_games_completed\n' +
+          '    }\n' +
+          '  }  \n' +
+          '\n' +
+          '\n' +
+          '\n',
+        variables: null
+      }),
+    }
+    const response = await fetch(URI.concat('/graphql'), graphOpts);
     /* istanbul ignore next */
     if (!response.ok) {
       throw new Error('Error fetching user data');
@@ -36,13 +80,16 @@ const HighScores = () => {
     return response.json();
   });
 
+  const bestBeginnerScore = userData?.data?.data?.completed?.best_beginner_score;
+  const bestIntermediateScore = userData?.data?.data?.completed?.best_intermediate_score;
+  const bestExpertScore = userData?.data?.data?.completed?.best_expert_score;
 
   return (
     <div className="best-scores">
       High Scores:
-      <BestIndividualScore type={beginner} score={userData?.data?.bestBeginnerScore || defaultScore} />
-      <BestIndividualScore type={intermediate} score={userData?.data?.bestIntermediateScore || defaultScore} />
-      <BestIndividualScore type={expert} score={userData?.data?.bestExpertScore || defaultScore} />
+      <BestIndividualScore type={beginner} score={bestBeginnerScore || defaultScore} />
+      <BestIndividualScore type={intermediate} score={bestIntermediateScore || defaultScore} />
+      <BestIndividualScore type={expert} score={bestExpertScore || defaultScore} />
       <BestGlobal type={beginner} scores={highScores?.data?.beginner || []} />
       <BestGlobal type={intermediate} scores={highScores?.data?.intermediate || []} />
       <BestGlobal type={expert} scores={highScores?.data?.expert || []} />
