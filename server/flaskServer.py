4f0d087f9d494af123aa43c7e436c05b89daaa26
jsmithb117 <jsmithb117@gmail.com>
2021-11-24 11:23:45 -0800

    Changes data shape to match client

--- server/flaskServer.py
+++ server/flaskServer.py
@@ -19,6 +19,24 @@ from pymongo import MongoClient
 client = MongoClient(port=27017)
 db = client.minesweeper
 
+
+
+# def to_json(score):
+#   # print(score)
+#   score_list = list(score)
+#   new_date = score['date'].isoformat()
+#   print('new_date')
+#   print(new_date)
+#   return dict(_id=score['_id']["$oid"],
+#     username=score[score_list[0]],
+#     seconds=score[score_list[1]],
+#     date=new_date,
+#   )
+
+
+# dbResponse = db.highscores.find_one({ "id": 1 })
+# # print(db)
+
 def parse_json(data):
   return json.loads(json_util.dumps(data))
 
@@ -29,7 +47,9 @@ def getBeginnerHighScores():
     score["_id"] = str(score["_id"])
     score["date"] = score["date"].isoformat()[:-3] + "Z"
 
-  return Response(json.dumps(parse_json(dbResponse)), mimetype='application/json')
+  parsedResponse = parse_json(dbResponse)
+
+  return Response(json.dumps(parsedResponse["beginner"]), mimetype='application/json')
 
 
 # /Temporary DB Stuff
